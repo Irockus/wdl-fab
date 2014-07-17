e4b4ccf37b74bf11f9fab7a7e1a9515a1cadadb9
fab672000 <fabien@onepost.net>
2014-07-17 12:10:35 -0500

    [CMake] Fixed out of source did for post build command copy and also for custom user project did auto detection

--- cleanup_build.sh
+++ cleanup_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find . -type d  \( -name '*.dir' -o -name '*.build' -o -name 'CMake*' -o -name 'x64*' -o -name 'Debug' -o -name 'Release' -o -iname 'PCH' \) -exec rm -rf "{}" \;
 find . -type f \( -iname 'cmake_install.*' -o -iname 'CMakeCache*' -o -name '*~' \) -exec rm "{}" \;
-rm -rf ./bin ./lib
+rm -rf ./bin ./lib build/osx build/win
